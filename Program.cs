2783c3c8a0464851998680bbe33a1bd4f4fe7c36
chillichillibang <79812978+chillichillibang@users.noreply.github.com>
2021-03-18 15:21:02 +1100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IntroToProgramming {
+    class Program {
+        static void Main(string[] args) {
+            
+            Console.WriteLine("How old are you? ");
+            string age = Console.ReadLine();
+
+            int ageNum = int.Parse(age);
+
+            if (ageNum >= 18) {
+                Console.WriteLine("Legally an adult");
+
+            }
+
+            else {
+                Console.WriteLine("Legally not adult age");
+
+            }  
+
+        }
+    }
+}
